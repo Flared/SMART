484b4fd3dc71e92705f2d66d1f2e4cd7c558dd91
Keith Richards <dkrichards@rti.org>
2017-08-03 15:03:04 -0400

    Updates run commands

--- backend/django/runserver.sh
+++ backend/django/runserver.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# wait for postgres to be ready
-nc -z postgres 5432
-n=$?
-while [ $n -ne 0 ]; do
-    sleep 1
-    nc -z postgres 5432
-    n=$?
-done
-
-python ./manage.py runserver 0.0.0.0:8000
+if [ "$SMART_ENV" = 'prod' ]; then
+	python manage.py collectstatic -c --noinput
+    gunicorn --bind=0.0.0.0:8000 -w 4 -k gevent -t 60 smart.wsgi
+else
+    python manage.py runserver 0.0.0.0:8000
+fi
